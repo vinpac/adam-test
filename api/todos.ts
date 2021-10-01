39084cdeac00535a4b44a5fc3c092d6f2ca1a743
Vinicius Pacheco <vin175pacheco@gmail.com>
2021-10-01 12:48:08 -0300

    first upadte

--- api/todos.ts
+++ api/todos.ts
@@ -1,9 +1,9 @@
 const sleep = () => new Promise((resolve) => setTimeout(resolve, 5000));
-let i = 1;
+let i = 10;
 
 export default async (req, res) => {
   res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=59");
-  const data = { adam: i };
+  const data = { adam: i, vini: i * -1 };
   await sleep();
   i += 1;
 
